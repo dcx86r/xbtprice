9648f6cfd0b58b1f5bfc9e47c49a6498f1160cf8
dcx86r <dcx86r@outlook.com>
2021-05-03 15:41:26 -0400

    Updated server app

--- app.pl
+++ app.pl
@@ -4,6 +4,7 @@ use File::Basename qw(dirname);
 use Cwd qw(abs_path);
 use lib ( dirname abs_path($0) ) . '';
 use Local::DB;
+use JSON;
 use Mojolicious::Lite;
 
 my $dbfile = $ARGV[0];
@@ -18,13 +19,14 @@ sub fetch {
 	for my $row (@rows) {
 		push @aoh, { time => @$row[0], value => (0 + @$row[1]) };
 	}
-	return Cpanel::JSON::XS->new->encode(\@aoh);
+	return JSON->new->encode(\@aoh);
 }
 
-get '/day' => sub {
+get '/' => sub {
 	my $c = shift;
 	$c->res->headers->access_control_allow_origin('*');
 	$c->render(text => fetch($dbfile), format => 'json');
 };
 
-app->start( 'daemon', '-l', 'http://*:3000' );
+#app->start( 'daemon', '-l', 'http://*:3000' );
+app->start('daemon', '-m', 'production', '-l', 'http+unix://%2Ftmp%2Fapp.sock', '-p');
